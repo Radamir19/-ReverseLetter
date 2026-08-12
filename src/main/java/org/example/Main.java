079a2667275beaf407eebab46bb60ca98dffdd9d
Radamir19 <radamir33@mail.ru>
2026-08-12 18:08:24 +0300

    Добавь StringUtil и вызов в Main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,27 +2,6 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         String example = "@#%#^%@&&^%f";
-        reverseString(example);
-    }
-
-    public static void reverseString(String str){
-        char[] charactersOfReversedString = str.toCharArray();
-        int left = 0;
-        int right = str.length() - 1;
-        while(left < right){
-            if(Character.isLetter(charactersOfReversedString[right]) && Character.isLetter(charactersOfReversedString[left])){
-                char temp = charactersOfReversedString[left];
-                charactersOfReversedString[left] = charactersOfReversedString[right];
-                charactersOfReversedString[right] = temp;
-                left++;
-                right--;
-            }else if(!Character.isLetter(charactersOfReversedString[left])){
-                left++;
-            }else{
-                right--;
-            }
-        }
-        String result = new String(charactersOfReversedString);
-        System.out.println(result);
+        StringUtil.reverseString(example);
     }
 }
---
+++ src/main/java/org/example/StringUtil.java
@@ -0,0 +1,24 @@
+package org.example;
+
+public class StringUtil {
+    public static void reverseString(String str){
+        char[] charactersOfReversedString = str.toCharArray();
+        int left = 0;
+        int right = str.length() - 1;
+        while(left < right){
+            if(Character.isLetter(charactersOfReversedString[right]) && Character.isLetter(charactersOfReversedString[left])){
+                char temp = charactersOfReversedString[left];
+                charactersOfReversedString[left] = charactersOfReversedString[right];
+                charactersOfReversedString[right] = temp;
+                left++;
+                right--;
+            }else if(!Character.isLetter(charactersOfReversedString[left])){
+                left++;
+            }else{
+                right--;
+            }
+        }
+        String result = new String(charactersOfReversedString);
+        System.out.println(result);
+    }
+}
